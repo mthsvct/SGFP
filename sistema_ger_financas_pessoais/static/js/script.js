eec4db02d85e0cc191b90f9372b3e077cb2d1047
bianca-01 <biancasousa439@gmail.com>
2022-09-13 15:26:38 -0300

    add graphs

--- sistema_ger_financas_pessoais/static/js/script.js
+++ sistema_ger_financas_pessoais/static/js/script.js
@@ -11,3 +11,70 @@ window.onload = function colcocaCor(){
         botaoCor.style.backgroundColor = cor;
     }
 }
+
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['mes','valor'],
+        ['Jan',100],
+        ['Fev',400],
+        ['Mar',250],
+        ['abr',500]
+    ]);
+
+    var options = {
+        title: 'Gastos por mês',
+        titleTextStyle: {
+            fontSize: 16,
+        
+          },
+          width: 600,
+          height: 400,
+        colors : ['#4BB9AF'],
+        legend: 'none'
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('gr1'));
+
+    chart.draw(data, options);
+
+    var data2 = google.visualization.arrayToDataTable([
+        ['Status', 'qnt'],
+        ['Disponivel', 100],
+        ['Usado',400]
+      ]);
+  
+      var options2 = {
+        title: 'Limite dos cartões',
+        width: 600,
+        height: 400,
+        colors: ['#AEEEE0','#4BB9AF'],
+       
+      };
+  
+      var chart2 = new google.visualization.PieChart(document.getElementById('gr2'));
+  
+      chart2.draw(data2, options2);
+
+      var data3 = google.visualization.arrayToDataTable([
+        ["Element", "Density" ],
+        ["1", 8.94],
+        ["2", 10.49],
+        ["3", 19.30],
+        ["4", 21.45]
+      ]);
+
+      var options3 = {
+        title: "Gastos por categoria",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+        colors: ['#AEEEE0']
+      };
+      var chart3 = new google.visualization.ColumnChart(document.getElementById("gr3"));
+      chart3.draw(data3, options3);
+}
